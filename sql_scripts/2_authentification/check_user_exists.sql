060005e79463d0b4a2de593cb0bbce96ed26dc92
Aya Lemzouri <136323763+Ayalem@users.noreply.github.com>
2025-11-12 13:54:27 +0100

    Add procedure to check if user exists

--- sql_scripts/2_authentification/check_user_exists.sql
+++ sql_scripts/2_authentification/check_user_exists.sql
@@ -1,1 +1,15 @@
+ CREATE OR REPLACE PROCEDURE check_user_exists(
+    p_user_id IN UTILISATEUR.user_id%TYPE
+)
+IS
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_count
+    FROM UTILISATEUR
+    WHERE user_id = p_user_id;
 
+    IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20030, 'Utilisateur introuvable.');
+    END IF;
+END;
+/
