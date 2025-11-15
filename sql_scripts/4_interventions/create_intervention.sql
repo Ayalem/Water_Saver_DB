ed930435aa4969572495bf096a796bec4568cb63
bluebeard3000 <zhour.ouihlane@edu.uiz.ac.ma>
2025-11-15 13:22:14 +0100

    Update create_intervention.sql

--- sql_scripts/4_interventions/create_intervention.sql
+++ sql_scripts/4_interventions/create_intervention.sql
@@ -1,1 +1,24 @@
+CREATE OR REPLACE TRIGGER trg_create_intervention_on_alerte
+AFTER INSERT ON ALERTE
+FOR EACH ROW 
+BEGIN
+    INSERT INTO INTERVENTION (
+         alerte_id,
+         parcelle_id,
+         capteur_id,
+         type_intervention,
+         priorité,
+         statut,
+         description)
+         VALUES (
+            :NEW.alerte_id,
+            :NEW.parcelle_id,
+            :NEW.capteur_id,
+            'INSPECTEUR',
+            'HAUTE',
+            'EN_ATTENTE',
+            'Intervention générée automatiquement suite à une alerte.');
+notifier_admin('Nouvelle alerte #'|| :NEW.alerte_id || 'intervention doit être assignée.');
+END;
+/
 
