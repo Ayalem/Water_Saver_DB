308cfad95e503d5abe6fd821b1d67771c1be57a3
Asmaakaraki887 <asmaakaraki741@gmail.com>
2025-11-15 13:05:19 +0100

    Add files via upload

--- sql_scripts/10_parcelle/SUPERFICIE_REELLE_CHAMP.sql
+++ sql_scripts/10_parcelle/SUPERFICIE_REELLE_CHAMP.sql
@@ -1,1 +1,19 @@
+CREATE OR REPLACE FUNCTION SUPERFICIE_REELLE_CHAMP(
+    p_champ_id IN NUMBER
+) RETURN NUMBER
+IS
+    v_superficie_totale NUMBER;
+BEGIN
+    -- Somme des superficies de toutes les parcelles actives du champ
+    SELECT NVL(SUM(superficie), 0)
+    INTO v_superficie_totale
+    FROM PARCELLE
+    WHERE champ_id = p_champ_id
+      AND statut = 'ACTIVE';
 
+    RETURN v_superficie_totale;
+EXCEPTION
+    WHEN OTHERS THEN
+        RETURN 0;
+END SUPERFICIE_REELLE_CHAMP;
+/
