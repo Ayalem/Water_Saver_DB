15d8c144883189bfc4192f01a2a1c48c28870471
Asmaakaraki887 <asmaakaraki741@gmail.com>
2025-11-22 15:54:45 +0100

    Rename parameter in GET_PARCELLE_BY_ID function

--- sql_scripts/10_parcelle/get_parcelle_id.sql
+++ sql_scripts/10_parcelle/get_parcelle_id.sql
@@ -1,17 +1,18 @@
 CREATE OR REPLACE FUNCTION GET_PARCELLE_BY_ID(
-    p_parcelle_id IN NUMBER
+    p_id IN NUMBER
 ) RETURN SYS_REFCURSOR
 IS
     v_cursor SYS_REFCURSOR;
 BEGIN
     OPEN v_cursor FOR
     SELECT p.*, c.nom as champ_nom, tc.nom as type_culture_nom,
-           CALCULER_RENDEMENT_PREVU(p.parcelle_id) as rendement_prevue,
-           EST_RECOLTE_IMMINENTE(p.parcelle_id) as recolte_imminente
+           CALCULER_RENDEMENT_PREVU(p.parcelle_id) AS rendement_prevue,
+           EST_RECOLTE_IMMINENTE(p.parcelle_id) AS recolte_imminente
     FROM PARCELLE p
     JOIN CHAMP c ON p.champ_id = c.champ_id
     LEFT JOIN TYPE_CULTURE tc ON p.type_culture_id = tc.type_culture_id
-    WHERE p.parcelle_id = p_parcelle_id;
-    
+    WHERE p.parcelle_id = p_id;
+
     RETURN v_cursor;
 END GET_PARCELLE_BY_ID;
+/
