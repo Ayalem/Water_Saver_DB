0b8dae5ba48ae1ec3db234647359000adcc58741
Aya Lemzouri <136323763+Ayalem@users.noreply.github.com>
2025-11-22 17:29:20 +0100

    Update error codes and add commit in create_user.sql

--- sql_scripts/2_authentification/create_user.sql
+++ sql_scripts/2_authentification/create_user.sql
@@ -10,21 +10,21 @@ CREATE OR REPLACE PROCEDURE create_user(
   v_count NUMBER;
   BEGIN 
      IF NOT REGEXP_LIKE(p_email,'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') THEN 
-         RAISE_APPLICATION_ERROR(-20000,'email invalide');
+         RAISE_APPLICATION_ERROR(-20099,'email invalide');
      END IF;
      SELECT COUNT(*) INTO v_count
      FROM UTILISATEUR
      WHERE email=p_email;
       
      IF v_count>0 THEN
-        RAISE_APPLICATION_ERROR(-200001,'email déja utilisé');
+        RAISE_APPLICATION_ERROR(-20098,'email déja utilisé');
      END IF;
    
      
      INSERT INTO UTILISATEUR(email,password_hash,nom,prenom,telephone,role,region_affectation)
      VALUES (p_email,p_password_hash,p_nom,p_prenom,p_telephone,p_role,p_region_affectation);
      DBMS_OUTPUT.PUT_LINE('Utilisateur créé avec succès : ' || p_email);
-     
+     COMMIT;
   END;
   
      
