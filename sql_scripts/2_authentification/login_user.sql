2201620951b06f3c2b6fa134c72f51aa7e329f77
Aya Lemzouri <136323763+Ayalem@users.noreply.github.com>
2025-11-23 12:17:16 +0100

    Implement account status checks in login_user.sql
    Added account status checks and error handling during user login.

--- sql_scripts/2_authentification/login_user.sql
+++ sql_scripts/2_authentification/login_user.sql
@@ -12,7 +12,12 @@ check_user_exists(p_email=>p_email);
     INTO v_user_rec
     FROM UTILISATEUR
     WHERE email = p_email;
-
+    
+      IF v_user_rec.statut = 'BLOQUE' THEN
+        RAISE_APPLICATION_ERROR(-20010, 'Votre compte est bloqué. Contactez un administrateur.');
+    ELSIF v_user_rec.statut = 'DESACTIVE' THEN
+        RAISE_APPLICATION_ERROR(-20011, 'Votre compte est désactivé.');
+    END IF;
     -- 2. Wrong password?
     IF v_user_rec.password_hash != p_password_hash THEN
 
@@ -22,13 +27,12 @@ check_user_exists(p_email=>p_email);
         UPDATE UTILISATEUR
         SET tentatives_echec = v_user_rec.tentatives_echec
         WHERE email = p_email;
+        COMMIT;
 
         -- 3. Block account after 5 failures
         IF v_user_rec.tentatives_echec >= 5 THEN
             update_statut_user(
                 p_user_id       => v_user_rec.user_id,
-                p_admin         => v_user_rec.user_id,    -- or admin_id if you want admin
-                p_password_hash => v_user_rec.password_hash,
                 p_statut        => 'BLOQUE'
             );
 
@@ -46,10 +50,23 @@ check_user_exists(p_email=>p_email);
         WHERE user_id = v_user_rec.user_id;
 
         DBMS_OUTPUT.PUT_LINE('Connexion réussie pour : ' || p_email);
+        COMMIT;
     END IF;
-
 EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20005, 'Email non trouvé.');
+
+    WHEN OTHERS THEN
+        ROLLBACK;
+
+        -- Si c'est déjà un RAISE_APPLICATION_ERROR → renvoyer tel quel
+        IF SQLCODE BETWEEN -20999 AND -20000 THEN
+            RAISE;
+        END IF;
+
+        RAISE_APPLICATION_ERROR(
+            -20090,
+            'Erreur lors du login : ' || SQLERRM
+        );
+
+       
 END;
 /
